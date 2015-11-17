abbcf5f1e0f4b46ea1250e82c076bb386127f9d0
Owen Yang <usay@outlook.com>
2015-11-17 17:58:03 +0800

    restructure code and demo component added

--- src/layouts/CoreLayout.js
+++ src/layouts/CoreLayout.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import 'styles/core.scss';
+import React from 'react'
+
+import Header from 'components/Header'
+
+import 'styles/core.scss'
 
 export default class CoreLayout extends React.Component {
   static propTypes = {
@@ -8,8 +11,9 @@ export default class CoreLayout extends React.Component {
 
   render () {
     return (
-      <div className='page-container'>
-        <div className='view-container'>
+      <div className='cdmetrics'>
+        <Header className='header'></Header>
+        <div className='content'>
           {this.props.children}
         </div>
       </div>
